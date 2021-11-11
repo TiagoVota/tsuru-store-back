fc332b167c2a1d43b01b0576904e5db102a7d8c6
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2021-11-11 12:50:22 -0300

    Adjust heroku and local host deploy; implment test verification pre-commit

--- src/database/database.js
+++ src/database/database.js
@@ -3,14 +3,29 @@ import pg from 'pg';
 const { Pool } = pg;
 
 
-const { DATABASE_URL } = process.env;
+const {
+  NODE_ENV,
+  DB_USER,
+  DB_PASS,
+  DB_PORT,
+  DB_HOST,
+  DB_NAME,
+} = process.env;
 
-const databaseConfig = {
-  connectionString: DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
+const databaseConfig = (NODE_ENV === 'production')
+  ? {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    }
   }
-};
+  : {
+    user: DB_USER,
+    password: DB_PASS,
+    port: DB_PORT,
+    host: DB_HOST,
+    database: DB_NAME
+  };
 
 const connection = new Pool (databaseConfig);
 

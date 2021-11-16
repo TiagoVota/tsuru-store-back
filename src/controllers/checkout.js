3cab6fe45cf332ac1c3dadb2225e9249b1d760e7
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2021-11-16 07:15:21 -0300

    Checkout refactoring

--- src/controllers/checkout.js
+++ src/controllers/checkout.js
@@ -24,73 +24,67 @@ const checkout = async (req, res) => {
       WHERE
         user_id = $1;
     `, [userId.rows[0].id]);
+    
+    if (cartId.rowCount === 0) return res.sendStatus(404);
 
-    if (cartId.rowCount !== 0) {
-      const productsInCart = await connection.query(`
-        SELECT
-          product_id, quantity
-        FROM 
-          carts_products
-        WHERE
-          cart_id = $1;
-      `, [cartId.rows[0].id]);
+    const productsInCart = await connection.query(`
+      SELECT
+        product_id, quantity
+      FROM 
+        carts_products
+      WHERE
+        cart_id = $1;
+    `, [cartId.rows[0].id]);
 
-      if (productsInCart.rowCount !== 0) {
-        await connection.query(`
-          INSERT INTO
-            sales (user_id, time)
-          VALUES 
-            ($1, $2);
-        `, [userId.rows[0].id, dayjs().format('DD/MM/YY')]);
+    if (productsInCart.rowCount === 0) return res.sendStatus(404);
 
-        const sales_id = await connection.query(`
-          SELECT
-            id
-          FROM
-            sales
-          WHERE
-            user_id = $1
-          ORDER BY
-            id DESC
-          LIMIT 1;
-        `, [userId.rows[0].id]);
+    await connection.query(`
+      INSERT INTO
+        sales (user_id, time)
+      VALUES 
+        ($1, $2);
+    `, [userId.rows[0].id, dayjs().format('DD/MM/YY')]);
 
-        productsInCart.rows.forEach(async (product) => {
-          await connection.query(`
-            INSERT INTO
-              sales_products (sale_id, product_id, quantity)
-            VALUES
-              ($1, $2, $3);
-          `, [sales_id.rows[0].id, product.product_id, product.quantity]);
-        });
+    const sales_id = await connection.query(`
+      SELECT
+        id
+      FROM
+        sales
+      WHERE
+        user_id = $1
+      ORDER BY
+        id DESC
+      LIMIT 1;
+    `, [userId.rows[0].id]);
 
-        await connection.query(`
-          DELETE FROM
-            carts_productS
-          WHERE
-            cart_id = $1;
-        `, [cartId.rows[0].id]);
+    productsInCart.rows.forEach(async (product) => {
+      await connection.query(`
+        INSERT INTO
+          sales_products (sale_id, product_id, quantity)
+        VALUES
+          ($1, $2, $3);
+      `, [sales_id.rows[0].id, product.product_id, product.quantity]);
+    });
 
-        await connection.query(`
-          DELETE FROM
-            carts
-          WHERE
-            user_id = $1;
-        `, [userId.rows[0].id]);
+    await connection.query(`
+      DELETE FROM
+        carts_products
+      WHERE
+        cart_id = $1;
+    `, [cartId.rows[0].id]);
 
-        res.sendStatus(200);
-        return;
-      }
-      res.sendStatus(404);
-      return;
-    }
-    res.sendStatus(404);
-    return;
+    await connection.query(`
+      DELETE FROM
+        carts
+      WHERE
+        user_id = $1;
+    `, [userId.rows[0].id]);
+
+    return res.sendStatus(200);
 
   } catch (error) {
     console.log(error);
-    res.sendStatus(500);
-    return;
+    return res.sendStatus(500);
   }
 };
 
